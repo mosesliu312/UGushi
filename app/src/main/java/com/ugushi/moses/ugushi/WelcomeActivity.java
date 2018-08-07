faecac542017a2cd20b0ec7eb6a43483e7ddde9f
mosesliu312 <mosesliu312@gmail.com>
2018-08-07 11:29:17 +0800

    8/7

--- app/src/main/java/com/ugushi/moses/ugushi/WelcomeActivity.java
+++ app/src/main/java/com/ugushi/moses/ugushi/WelcomeActivity.java
@@ -1,8 +1,12 @@
 package com.ugushi.moses.ugushi;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 public class WelcomeActivity extends Activity {
 
@@ -10,5 +14,15 @@ public class WelcomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        /*
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+        */
     }
 }
